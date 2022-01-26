279026b9d68e4f09b1f287d3abc8b9968ec32dd5
TCFTEPEKOY <Xswedc12w>
2022-01-26 14:36:33 +0300

    sdsd

--- src/main/java/com/iyg/bankingclan/BankingClanApplication.java
+++ src/main/java/com/iyg/bankingclan/BankingClanApplication.java
@@ -1,5 +1,10 @@
 package com.iyg.bankingclan;
 
+import com.iyg.bankingclan.model.AccountModel;
+import com.iyg.bankingclan.model.ActiveAccount;
+import com.iyg.bankingclan.model.Bank;
+import com.iyg.bankingclan.model.BankUsa;
+import com.iyg.bankingclan.model.Currency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,10 @@ public class BankingClanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingClanApplication.class, args);
+        AccountModel accountModel = new ActiveAccount();
+        BankUsa bankUSA = new BankUsa();
+        bankUSA.getDollarAccountAdapter().getBalanceInDollars(accountModel);
+        bankUSA.getDollarAccountAdapter().getBalanceInDifferentCurrency(accountModel, new Currency());
     }
 
 }
