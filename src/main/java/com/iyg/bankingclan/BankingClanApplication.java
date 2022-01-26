14415d050e1a31e744c6e5a314d196a779b4a595
TCFTEPEKOY <Xswedc12w>
2022-01-26 15:52:35 +0300

    Adapter + Facade

--- src/main/java/com/iyg/bankingclan/BankingClanApplication.java
+++ src/main/java/com/iyg/bankingclan/BankingClanApplication.java
@@ -5,6 +5,7 @@ import com.iyg.bankingclan.model.ActiveAccount;
 import com.iyg.bankingclan.model.Bank;
 import com.iyg.bankingclan.model.BankUsa;
 import com.iyg.bankingclan.model.Currency;
+import com.iyg.bankingclan.model.MainAccount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,10 +14,13 @@ public class BankingClanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingClanApplication.class, args);
-        AccountModel accountModel = new ActiveAccount();
-        BankUsa bankUSA = new BankUsa();
-        bankUSA.getDollarAccountAdapter().getBalanceInDollars(accountModel);
-        bankUSA.getDollarAccountAdapter().getBalanceInDifferentCurrency(accountModel, new Currency());
+//        AccountModel accountModel = new ActiveAccount();
+//        BankUsa bankUSA = new BankUsa();
+//        bankUSA.getDollarAccountAdapter().getBalanceInDollars(accountModel);
+//        bankUSA.getDollarAccountAdapter().getBalanceInDifferentCurrency(accountModel, new Currency(2));
+        MainAccount mainAccount = new MainAccount();
+        AccountModel model1 = new ActiveAccount();
+        mainAccount.getBanks().add(new BankUsa())
     }
 
 }
