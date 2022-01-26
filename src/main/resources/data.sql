c8e2bc2d405e822de729f07cb33ccb3160efe781
İlker Turan <73020372+ilkeryolundagerek@users.noreply.github.com>
2022-01-26 09:35:57 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO ACCOUNT (id, name, balance) values (1000, 'Dinesh', 20000);
-INSERT INTO ACCOUNT (id, name, balance) values (2000, 'Arnav', 40000);
-INSERT INTO ACCOUNT (id, name, balance) values (3000, 'Anamika', 60000);
+INSERT INTO ACCOUNT (id, name, balance) values (1000, 'John Doe', 20000);
+INSERT INTO ACCOUNT (id, name, balance) values (2000, 'Jane Doe', 40000);
+INSERT INTO ACCOUNT (id, name, balance) values (3000, 'Jimmy Doe', 60000);
